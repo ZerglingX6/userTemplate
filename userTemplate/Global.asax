7307c98c02f54cf2cd44fa0c431361765638c3ec
zhouxinxin <zhouxinxinbit@163.com>
2017-07-12 13:53:22 +0800

    添加项目文件。

---
+++ userTemplate/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="userTemplate.MvcApplication" Language="C#" %>
---
+++ userTemplate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(userTemplate.Startup))]
+namespace userTemplate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
